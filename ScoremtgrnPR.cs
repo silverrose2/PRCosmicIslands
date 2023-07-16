fa4bd64e183f9419472f2b91196648d760066134
silverrose2 <126685934+silverrose2@users.noreply.github.com>
2023-07-16 04:36:19 +0100

    Update ScoremtgrnPR.cs

--- ScoremtgrnPR.cs
+++ ScoremtgrnPR.cs
@@ -3,13 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;//for health bar ui
 using TMPro;
-
-
-
-
 public class ScoremtgrnPR : MonoBehaviour// for mountain green level
 {
-
 
     public static int scoreValue = 0;
     public GameObject Caveentrance;//portal levelselect gameobj
@@ -19,26 +14,20 @@ public class ScoremtgrnPR : MonoBehaviour// for mountain green level
     public GameObject Portalcomp2;//portal levelselect gameobj
     public GameObject Portalcomp3;//portal levelselect gameobj
 
-
     [SerializeField]
     private int scorevaluefield;
-
 
     TextMeshProUGUI score;
     // Start is called before the first frame update
     void Start()
     {
         score = GetComponent<TextMeshProUGUI>();// score new************* trsmeshprogui replaces the usual old word Text
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
         score.text = "Score " + scoreValue;// score new ************
-
-
     }
 
     // void OnGui()// ON RENDER WONT WORK WITH CO ROUTINES BUT WORKS ITS FOR GUI TEXT
@@ -51,16 +40,10 @@ public class ScoremtgrnPR : MonoBehaviour// for mountain green level
             //ScoremtgrnPR.scoreValue += (10);// shouldnt be here oops so can vary oops wrong  this goes on enemy is dead function look carefully here your grabbing a script value sino must have scroemtgrn now
             Caveentrance.SetActive(true);
             Proceed.SetActive(true);// will allow entry to next level after 30 points
-
             Portalcomp1.SetActive(true);// will allow entry to next level after 30 points
             Portalcomp2.SetActive(true);// will allow entry to next level after 30 points
             Portalcomp3.SetActive(true);// will allow entry to next level after 30 points
-
-
-
             // dont destroy game obj just yet to decide
-
-
             // GetComponent<BosshealthPR>().Victory.IsActivetrue);// grabs you boss health component and says game over boss not out til 65 sec
             // next try set boss active so it flows
             // get portal open cave enrtrance
