21bfe6f077296f13576c2cd3de84a4881b4ac498
silverrose2 <126685934+silverrose2@users.noreply.github.com>
2023-07-16 04:24:33 +0100

    Update EnemydamagertogivetoplayerPR.cs

--- EnemydamagertogivetoplayerPR.cs
+++ EnemydamagertogivetoplayerPR.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemydamagetogivetoplayerPR : MonoBehaviour// This scrip attaches to a bullet. ref what bullet is hitting for damage
+public class EnemydamagetogivetoplayerPR : MonoBehaviour// This script attaches to a bullet. ref what bullet is hitting for damage
 {// Dont forget tag Enemy in Unity as Enemy *****************************
     public int damageToGive;// access to enemy health pay attention to the upper and lower case formats
     // Start is called before the first frame update
